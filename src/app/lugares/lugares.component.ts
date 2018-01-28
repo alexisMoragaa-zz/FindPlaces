1998531eefa4bfb1332e592f52cfc90c61dbd007
alexis2121 <alexis.moraga.gallardomail.com>
2018-01-28 14:28:07 -0300

    se añaden peticiones http en nuestro proyecto angular, se reemplazan temporalmente las peticiones por web sockets y se añaden las peticiones de tipo get y post a firebase. se añade una funcion para controlar y mostrar lso errrores por pantalla. fin etapa http curso angular 4

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -11,12 +11,24 @@ export class LugaresComponent {
   lat:number=-33.3898208;//latitud para cargar el marcador en el mapa
   lng:number=-70.64833810000005;//longitud para cargar el marcador en el mapa
   lugares = null;
-constructor(lugaresSer: LugaresService){
-  // this.lugares = lugaresSer.getLugares();
-  lugaresSer.getLugares()
-    .valueChanges().subscribe(lugares => {
-      
-      this.lugares = lugares;
+  errorText='';
+  constructor(lugaresSer: LugaresService){
+    // this.lugares = lugaresSer.getLugares();
+    lugaresSer.getLugares()
+      .subscribe(lugares => {//cuando usamos la peticion get mediante http quitamos el ValueChanges().subscribe()
+
+        // this.lugares = lugares;//obtenemos mediante web sockets
+
+        this.lugares = lugares;//cuando hacemos la peticion completa y formateamos la peticion con el operador map, este ya conbierte
+        // el objeto a json por nosotros, es por esto que cambiamos nuestra linea de this.lugares = lugares.json(); a this.lugares = lugares;
+
+        var me = this;//para no perder el scope de la variable this guardamos el mismo en una variable para posteriormente ser usada
+        me.lugares = Object.keys(me.lugares).map(function (key) { return me.lugares[key]; });//combertimos de un objetoa un array, para posteriormente poder iterarlo
+
+    },
+    error=>{
+      console.log(error);
+      this.errorText='Actualmente tenemos Dificultades para mostrar la Informacion. Error '+error.statusText;
     });
-}
+  }
 }
--- src/app/services/lugares.service.ts
+++ src/app/services/lugares.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from'angularfire2/database';
-import { Http } from "@angular/http";
+import { Http, Headers } from "@angular/http";
+import 'rxjs/add/operator/map';//este import corresponde  a un operador, es por esta razon que se importa de esta forma
 
 @Injectable()
 export class LugaresService{
-
+API_ENDPOINT ="https://cursoangular4-1516112349964.firebaseio.com";
   // lugares:any=[
   //   {id: 1, plan:'pagado',cercania:1, distancia:1, nombre:'Floreia la Gardenia', active:true,description:"esta es una descripcion generica que aplica a todos los puntos actualmente"},
   //   {id: 2, plan:'pagado',cercania:1, distancia:1.8, nombre:'Donas la Pasadita', active:true,description:"esta es una descripcion generica que aplica a todos los puntos actualmente"},
@@ -17,16 +18,25 @@ constructor(private afDB:AngularFireDatabase, private http:Http){}
 
 
   public getLugares(){
-    return this.afDB.list('lugares/');
+    // return this.afDB.list('lugares/');//obtenemos lugares mediante websockets
+    // return this.http.get(this.API_ENDPOINT+'/lugares.json');//obtenemos el objeto lugares mediante http
+    return this.http.get(this.API_ENDPOINT+'/.json')//obtenemos todo lo que tenemos en firebase, para esto necesitamos dar formato anuestra respuesta  es por esto que usamos el operador map
+      .map((resultado)=>{
+      const data = resultado.json().lugares;
+      return data;
+  })
   }
 
   public getLugar(id){
     return this.afDB.object('lugares/'+id);
   }
 
+
   public guardarLugar(lugar){
     console.log(lugar);
-    this.afDB.database.ref('lugares/'+lugar.id).set(lugar);
+    // this.afDB.database.ref('lugares/'+lugar.id).set(lugar);//llamado por wb sockets
+    const headers = new Headers({"Content-Type":"application/json"});//cabecera de la peticion http
+    return this.http.post(this.API_ENDPOINT+'/lugares.json',lugar,{headers:headers}).subscribe();//peticion http
   }
   public editarLugar(lugar){
     console.log(lugar);
